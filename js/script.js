9c94e99f0ab4d7b1dc48003be5c5b5502cdc539b
lucky-loch <57145684+lucky-loch@users.noreply.github.com>
2022-04-19 00:33:56 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,58 +1,74 @@
 // JavaScript Document
-function windowScroll() {
-  const navbar = document.getElementById("navbar");
-  if (
-    document.body.scrollTop >= 50 ||
-    document.documentElement.scrollTop >= 50
-  ) {
-    navbar.classList.add("nav-sticky");
-  } else {
-    navbar.classList.remove("nav-sticky");
-  }
-}
- window.addEventListener("scroll", (ev) => {
-  ev.preventDefault()
-  windowScroll()
+// Smooth Anchor Scrolling
+$('a').click(function() {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 300);
+    return false;
 });
 
-$('.navbar-collapse a').click(function(){
-            $(".navbar-collapse").collapse('hide');
-        });
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+        $('.kk').addClass('kk2 shadow-sm');
+    } else {
+        $('.kk').removeClass('kk2 shadow-sm');
+    }
+});
+
+function windowScroll() {
+    const navbar = document.getElementById("navbar");
+    if (
+        document.body.scrollTop >= 50 ||
+        document.documentElement.scrollTop >= 50
+    ) {
+        navbar.classList.add("nav-sticky");
+    } else {
+        navbar.classList.remove("nav-sticky");
+    }
+}
+window.addEventListener("scroll", (ev) => {
+    ev.preventDefault()
+    windowScroll()
+});
+
+$('.navbar-collapse a').click(function() {
+    $(".navbar-collapse").collapse('hide');
+});
 // init Isotope
 var $grid = $('.grid').isotope({
-  itemSelector: '.element-item',
-  layoutMode: 'fitRows'
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
 });
 // filter functions
 var filterFns = {
-  // show if number is greater than 50
-  numberGreaterThan50: function() {
-    var number = $(this).find('.number').text();
-    return parseInt( number, 10 ) > 50;
-  },
-  // show if name ends with -ium
-  ium: function() {
-    var name = $(this).find('.name').text();
-    return name.match( /ium$/ );
-  }
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+        var number = $(this).find('.number').text();
+        return parseInt(number, 10) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+        var name = $(this).find('.name').text();
+        return name.match(/ium$/);
+    }
 };
 // bind filter button click
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
+$('.filters-button-group').on('click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[filterValue] || filterValue;
+    $grid.isotope({ filter: filterValue });
 });
 // change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
+$('.button-group').each(function(i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+    });
 });
 
-   $(document).ready(function () {
+$(document).ready(function() {
     $('.navbar-nav li a').click(function(e) {
 
         $('.navbar-nav li').removeClass('active');
@@ -65,19 +81,19 @@ $('.button-group').each( function( i, buttonGroup ) {
 });
 
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:false,
-    dots:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:1
+        600: {
+            items: 1
         },
-        1000:{
-            items:1
+        1000: {
+            items: 1
         }
     }
 })
